a61e242d72b9ca27039ae6f7057c5d1b15857595
shanguanma <nanr9544@gmail.com>
2025-06-03 16:56:28 +0800

    remove unusefull file

--- egs/alimeeting/ts_vad2/prepare_alimeeting_target_audio_list.py
+++ egs/alimeeting/ts_vad2/prepare_alimeeting_target_audio_list.py
@@ -1,1 +1,20 @@
-../ts_vad_ddp_phase1/prepare_alimeeting_target_audio_list.py
+#!/usr/bin/env python
+
+import os
+
+import sys
+import glob
+
+def main():
+    input_dir=sys.argv[1]
+    output = sys.argv[2]
+    #input_dir=
+    with open(output,'w')as fw:
+        #for x in os.listdir(input_dir):
+        for x in glob.glob(f"{input_dir}/*/*.wav"):
+            if 'all' not in x:
+                print(f"x: {x}!")
+                fw.write(f"{x}\n")
+
+if __name__=="__main__":
+    main()
