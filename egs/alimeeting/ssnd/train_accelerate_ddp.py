652e69e9f102cff52270764c3dcbd437b85ead07
shanguanma <maduo25@163.com>
2025-07-01 19:39:50 +0800

    fixed bugs

--- egs/alimeeting/ssnd/train_accelerate_ddp.py
+++ egs/alimeeting/ssnd/train_accelerate_ddp.py
@@ -944,8 +944,8 @@ def main():
         training=True
     )
     # 强制初始化DetectionDecoder输出层bias为0
-    with torch.no_grad():
-        model.det_decoder.out_proj.bias.fill_(0.0)
+    #with torch.no_grad():
+    #    model.det_decoder.out_proj.bias.fill_(0.0)
 
     logging.info(f"model: {model}")
     num_param = sum([p.numel() for p in model.parameters()])
