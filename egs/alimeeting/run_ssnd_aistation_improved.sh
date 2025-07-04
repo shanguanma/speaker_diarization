e5ccf99a4492161a81761d2b38fd88540465b3e2
shanguanma <maduo25@163.com>
2025-07-04 11:10:49 +0800

    update

--- egs/alimeeting/run_ssnd_aistation_improved.sh
+++ egs/alimeeting/run_ssnd_aistation_improved.sh
@@ -15,8 +15,8 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ];then
    valid_textgrid_dir=/maduo/datasets/alimeeting/Eval_Ali/Eval_Ali_far/textgrid_dir
    speaker_pretrain_model_path=/maduo/model_hub/speaker_pretrain_model/zh_cn/modelscope/speech_campplus_sv_zh-cn_16k-common/campplus_cn_common.bin
    extractor_model_type='CAM++_wo_gsp'
-   out_bias=-0.8
-   exp_dir=/maduo/exp/speaker_diarization/ssnd/ssnd_alimeeting_improved_lr1e-4_batch64_out_bias${out_bias}
+   out_bias=0.0
+   exp_dir=/maduo/exp/speaker_diarization/ssnd/ssnd_alimeeting_improved_lr5e-5_batch64_out_bias${out_bias}
    CUDA_VISIABLE_DEVICES=0,1\
   TORCH_DISTRIBUTED_DEBUG=DETAIL accelerate launch --main_process_port 15915 \
    ssnd/train_accelerate_ddp.py\
@@ -28,7 +28,7 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ];then
     --keep-last-k 1\
     --keep-last-epoch 1\
     --grad-clip true\
-    --lr 1e-4\
+    --lr 2e-5\
     --exp-dir $exp_dir\
     --train_wav_dir $train_wav_dir\
     --train_textgrid_dir $train_textgrid_dir\
@@ -40,6 +40,7 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ];then
     --speaker_pretrain_model_path $speaker_pretrain_model_path\
     --extractor_model_type $extractor_model_type\
     --out-bias $out_bias\
+    --warmup-updates 2000
     #--musan-path $musan_path \
     #--rir-path $rir_path 
      
--- egs/alimeeting/ssnd/train_accelerate_ddp.py
+++ egs/alimeeting/ssnd/train_accelerate_ddp.py
@@ -572,7 +572,7 @@ def train_one_epoch(
         if params.grad_clip:
             if accelerator.sync_gradients:
                 grad_norm = accelerator.clip_grad_norm_(
-                    model.parameters(), max_norm=10.0
+                    model.parameters(), max_norm=1.0
                 )
 
         optimizer.step()
