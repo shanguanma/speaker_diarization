5260750cc5cfc0a835a2232a1911e5b305ad0079
shanguanma <maduo25@163.com>
2025-07-09 15:32:33 +0800

    remove bias params

--- egs/alimeeting/run_ssnd_aistation_improved.sh
+++ egs/alimeeting/run_ssnd_aistation_improved.sh
@@ -15,9 +15,9 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ];then
    valid_textgrid_dir=/maduo/datasets/alimeeting/Eval_Ali/Eval_Ali_far/textgrid_dir
    speaker_pretrain_model_path=/maduo/model_hub/speaker_pretrain_model/zh_cn/modelscope/speech_campplus_sv_zh-cn_16k-common/campplus_cn_common.bin
    extractor_model_type='CAM++_gsp'
-   out_bias=-0.5
+   #out_bias=-0.5
    mask_prob=0.5
-   exp_dir=/maduo/exp/speaker_diarization/ssnd/ssnd_alimeeting_improved_lr1e-4_batch64_out_bias${out_bias}_mask_prob_${mask_prob}
+   exp_dir=/maduo/exp/speaker_diarization/ssnd/ssnd_alimeeting_improved_lr1e-4_batch64_mask_prob_${mask_prob}
    CUDA_VISIABLE_DEVICES=0,1\
   TORCH_DISTRIBUTED_DEBUG=DETAIL accelerate launch --main_process_port 15915 \
    ssnd/train_accelerate_ddp.py\
@@ -40,8 +40,9 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ];then
     --mask-prob $mask_prob\
     --speaker_pretrain_model_path $speaker_pretrain_model_path\
     --extractor_model_type $extractor_model_type\
-    --out-bias $out_bias\
     --warmup-updates 3000
+    #--out-bias $out_bias\
+    #--warmup-updates 3000
     #--musan-path $musan_path \
     #--rir-path $rir_path
 
--- egs/alimeeting/ssnd/train_accelerate_ddp.py
+++ egs/alimeeting/ssnd/train_accelerate_ddp.py
@@ -211,7 +211,7 @@ def get_parser():
     parser.add_argument('--max-seq-len', type=int, default=200)
     #parser.add_argument('--n-all-speakers', type=int, default=2097) # Alimeeting has 2097 speakers
     parser.add_argument('--mask-prob', type=float, default=0.5)
-    parser.add_argument('--out-bias', type=float, default=-0.5, help="output bias of detection decoder, >0 means more confident positive samples, <0 means more confident negative samples")
+    #parser.add_argument('--out-bias', type=float, default=-0.5, help="output bias of detection decoder, >0 means more confident positive samples, <0 means more confident negative samples")
     add_finetune_arguments(parser)
     return parser
 
@@ -1041,7 +1041,7 @@ def main():
         n_all_speakers=params.n_all_speakers,
         mask_prob=params.mask_prob,
         training=True,
-        out_bias=params.out_bias,
+        #out_bias=params.out_bias,
     )
     # 强制初始化DetectionDecoder输出层bias为0
     #with torch.no_grad():
