fb4e23fef93c86b6c379483723e8506e05c4658d
shanguanma <maduo25@163.com>
2025-08-14 18:20:00 +0800

    init add kimi_audio

---
+++ egs/mlc_slm/kimi_audio_fusion/datapipe.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025, Xingchen Song(sxc19@tsinghua.org.cn)
+
+import json
+from typing import Any, Dict
+
+import numpy
+import torch
+from torch.distributed.checkpoint.stateful import Stateful
+from torch.utils.data import IterableDataset
+
+from dataconfig import DataConfig
+from dataset import TouchDataset
+
+
+class LowLevelTouchDatapipe(IterableDataset, Stateful):
+    """The high-level interface dataset class
+
+        We have two shuffle stage in the Dataset. The first is global
+        shuffle at list level. The second is global shuffle
+        at training samples level.
+    """
+
+    def __init__(self, config: DataConfig, dp_rank: int, dp_world_size: int):
+        super().__init__()
+        self.lists = []
+        with open(config.datalist_path, "r") as f:
+            lists = f.readlines()
+            for line in lists:
+                line = line.strip().split()
+                assert len(line) == 2
+                self.lists.append(dict(dir=line[0], datatypes=line[1]))
+        self.config = config
+        self.dp_rank = dp_rank
+        self.dp_world_size = dp_world_size
+
+        # Variables for checkpointing
+        self.epoch = 0
+        self.consumed_lists = 0
+        self.consumed_samples = 0
+
+    def load_state_dict(self, state_dict: Dict[str, Any]):
+        self.epoch = state_dict["epoch"]
+        self.consumed_lists = state_dict["consumed_lists"]
+        self.consumed_samples = state_dict["consumed_samples"]
+
+    def state_dict(self) -> Dict[str, Any]:
+        return {
+            "epoch": self.epoch,
+            "consumed_lists": self.consumed_lists,
+            "consumed_samples": self.consumed_samples,
+        }
+
+    def __iter__(self):
+        while self.epoch < self.config.datalist_epoch:
+            list_idxs = list(range(len(self.lists)))
+
+            # 1st shuffle on lists
+            if self.config.datalist_shuffling:
+                g = torch.Generator()
+                g.manual_seed(self.epoch)
+                list_idxs = torch.randperm(len(self.lists), generator=g).tolist()
+
+            # 1st sharding on dp ranks
+            if self.config.datalist_sharding:
+                assert len(list_idxs) >= self.dp_world_size, \
+                    f"len(list_idxs) = {len(list_idxs)}, it should be equal or larger than dp_world_size = {self.dp_world_size}"
+                list_idxs = list_idxs[self.dp_rank::self.dp_world_size]
+
+            # 2nd sharding on dataloader workers
+            worker_info = torch.utils.data.get_worker_info()
+            if worker_info is None:
+                worker_id = 0
+                num_workers = 1
+            else:
+                worker_id = worker_info.id
+                num_workers = worker_info.num_workers
+            if self.config.datalist_epoch > 1:
+                assert len(list_idxs) >= num_workers, \
+                    f"len(list_idxs) = {len(list_idxs)}, it should be equal or larger than num_workers = {num_workers}"
+            list_idxs = list_idxs[worker_id::num_workers]
+
+            start_list = self.consumed_lists
+            for list_idx in list_idxs[start_list:]:
+                _dataset = TouchDataset(self.lists[list_idx]["dir"],
+                                        self.config.dataset_mmap,
+                                        self.lists[list_idx]["datatypes"])
+
+                # 2nd shuffle on samples
+                num_samples = len(_dataset)
+                g = torch.Generator()
+                g.manual_seed(self.epoch + self.consumed_lists)
+                if self.config.dataset_shuffling:
+                    sample_idxs = torch.randperm(num_samples, generator=g).tolist()
+                else:
+                    sample_idxs = list(range(num_samples))
+
+                start_sample = self.consumed_samples
+                for sample_idx in sample_idxs[start_sample:]:
+                    if self.lists[list_idx]["datatypes"] == "metainfo":
+                        # for text pre-training
+                        metainfo = _dataset.get(sample_idx, "metainfo")
+                        metainfo = metainfo.tobytes().decode('utf-8')
+                        metainfo = json.loads(metainfo.strip())
+                        metainfo["datatypes"] = "metainfo"
+                        yield metainfo
+                    elif self.lists[list_idx]["datatypes"] == "texttoken":
+                        # for text pre-training
+                        texttoken = _dataset.get(sample_idx, "texttoken").tolist()
+                        yield dict(input_ids=texttoken, datatypes="texttoken")
+                    elif self.lists[list_idx]["datatypes"] == "audio+metainfo":
+                        # for audio pre-training OR audio-text alignment
+                        """NOTE(xcsong) Example metainfo:
+                        {
+                            "key": "BAC009S0002W0122",
+                            "wav": "/jfs-hdfs/Aishell/train/S0002/BAC009S0002W0122.wav",
+                            "txt": "而对楼市成交抑制作用最大的限购",
+                            "sample_rate": 16000,
+                            "info": {
+                                "segments": [
+                                    {"start": 0.0, "end": 1.0, "txt": "而对"},
+                                    {"start": 1.0, "end": 2.0, "txt": "楼市"},
+                                    ...
+                                ],
+                                ...
+                                "xx": "yy"
+                            }
+                        }
+                        """
+                        metainfo = _dataset.get(sample_idx, "metainfo")
+                        metainfo = metainfo.tobytes().decode('utf-8')
+                        metainfo = json.loads(metainfo.strip())
+                        offset = 0
+                        length = None
+                        sample_rate = metainfo["sample_rate"]
+                        info = metainfo.get("info", None)
+                        if info is not None and self.config.dataset_load_audio_via_segments:
+                            # used in audio sft, like asr or tts
+                            segments = info.get("segments", None)
+                            # TODO(xcsong): Add arg to control segment selection
+                            if segments is not None:
+                                g = torch.Generator()
+                                g.manual_seed(self.epoch + self.consumed_lists + self.consumed_samples)
+                                segment = segments[torch.randint(len(segments), (1,), generator=g).item()]
+                                start = int(float(segment["start"]) * sample_rate)
+                                end = int(float(segment["end"]) * sample_rate)
+                                offset = start
+                                length = end - start
+                                metainfo['txt'] = segment['txt']
+                        if self.config.dataset_random_cut_audio:
+                            # used in audio pretrain
+                            _, total_length = _dataset.get_idx(sample_idx, "audio")
+                            min_length = self.config.dataset_random_cut_audio_min_length_in_ms / 1000.0 * sample_rate
+                            max_length = self.config.dataset_random_cut_audio_max_length_in_ms / 1000.0 * sample_rate
+                            assert max_length > min_length
+                            if total_length > min_length:
+                                g = torch.Generator()
+                                g.manual_seed(self.epoch + self.consumed_lists + self.consumed_samples)
+                                length = torch.randint(
+                                    low=int(min_length), high=min(total_length, int(max_length)),
+                                    size=(1,), generator=g).item()
+                                offset = torch.randint(
+                                    low=0, high=max(1, total_length - length),
+                                    size=(1,), generator=g).item()
+                        audio = _dataset.get(sample_idx, "audio", offset=offset, length=length)
+                        audio = audio.astype(numpy.float32) / 32768.0  # normalize to [-1.0, 1.0]
+                        metainfo["waveform"] = torch.from_numpy(audio).unsqueeze(0)  # [1, T]
+                        metainfo["datatypes"] = "audio+metainfo"
+                        yield metainfo
+                    else:
+                        raise NotImplementedError(f"unsupported datatypes: {self.lists[list_idx]['datatypes']}")
+                    self.consumed_samples += 1
+
+                self.consumed_samples = 0
+                self.consumed_lists += 1
+
+            # Reset states
+            self.consumed_samples = 0
+            self.consumed_lists = 0
+            self.epoch += 1
+
+
+class MidLevelTouchDatapipe(IterableDataset, Stateful):
+    """
+    MidLevelTouchDatapipe class for data processing.
+    """
+
+    def __init__(self, source, f, *args, **kw):
+        assert callable(f)
+        self.source = source
+        self.f = f
+        self.args = args
+        self.kw = kw
+
+    def __iter__(self):
+        """ Return an iterator over the source dataset processed by the
+            given functions (self.f).
+        """
+        assert self.source is not None
+        assert callable(self.f)
+        return self.f(iter(self.source), *self.args, **self.kw)
+
+    def apply(self, f):
+        assert callable(f)
+        return MidLevelTouchDatapipe(self, f, *self.args, **self.kw)
+
+    def load_state_dict(self, state_dict: Dict[str, Any]):
+        assert self.source is not None
+        self.source.load_state_dict(state_dict)
+
+    def state_dict(self) -> Dict[str, Any]:
+        assert self.source is not None
+        return self.source.state_dict()
---
+++ egs/mlc_slm/kimi_audio_fusion/tokenizer.py
@@ -0,0 +1,334 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023, NVIDIA CORPORATION (Megatron-LM teams).
+#               2025, Xingchen Song(sxc19@tsinghua.org.cn)
+
+"""Touch tokenizers."""
+
+import json
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+from typing import Any, Union
+
+import numpy
+import torch
+import torch.nn.functional as F
+import transformers
+
+from touchnet.tokenizer import TokenizerConfig
+
+
+class BaseTokenizer(ABC):
+    """Abstract class for tokenizer
+
+    Absent a config or class-specific tracking of which objects are uniquely identifying, we must
+    include all key word arguments as unique identifiers
+
+    Args:
+        tokenizer_paths (Tuple[str]): All tokenizer source paths or prefixes
+
+        tokenizer_options (Dict[str, Any]): All tokenizer options
+    """
+
+    def __init__(self, *tokenizer_paths: str, **tokenizer_options: Any):
+
+        self.unique_identifiers = OrderedDict()
+        self.unique_identifiers["class"] = type(self).__name__
+        self.unique_identifiers["tokenizer_path"] = list(tokenizer_paths)
+        for option in tokenizer_options:
+            self.unique_identifiers[option] = str(tokenizer_options[option])
+
+        self.unique_description = json.dumps(self.unique_identifiers, indent=4)
+
+        super().__init__()
+
+    @abstractmethod
+    def tokenize(self, inputs: Any) -> Union[numpy.ndarray, torch.Tensor]:
+        """Convert text/audio/video to embedding ids
+
+        Args:
+            inputs (Any): The text/audio/video to convert
+
+        Returns:
+            numpy.ndarray/torch.Tensor: The converted embedding ids
+        """
+        pass
+
+    def detokenize(self, ids: Union[numpy.ndarray, torch.Tensor]) -> Any:
+        """Convert embedding ids to text/audio/video
+
+        Args:
+            ids (numpy.ndarray/torch.Tensor): The ids to convert
+
+        Returns:
+            Any: The converted text/audio/video
+
+        Raises:
+            NotImplementedError: Non-abstract, optional method
+        """
+        raise NotImplementedError(
+            "{} has no method 'detokenize'".format(type(self).__name__)
+        )
+
+    @property
+    @abstractmethod
+    def vocab(self):
+        """Dictionary from vocab text token to id token"""
+        pass
+
+    @property
+    @abstractmethod
+    def inv_vocab(self):
+        """Dictionary from vocab id token to text token"""
+        pass
+
+    @property
+    @abstractmethod
+    def vocab_size(self):
+        """The vocabulary size"""
+        pass
+
+    @property
+    def cls(self):
+        """The CLS token id
+
+        Raises:
+            NotImplementedError: Non-abstract, optional attribute
+        """
+        raise NotImplementedError(
+            "{} has no attribute 'cls'".format(type(self).__name__)
+        )
+
+    @property
+    def sep(self):
+        """The SEP token id
+
+        Raises:
+            NotImplementedError: Non-abstract, optional attribute
+        """
+        raise NotImplementedError(
+            "{} has no attribute 'sep'".format(type(self).__name__)
+        )
+
+    @property
+    def pad(self):
+        """The PAD token id
+
+        Raises:
+            NotImplementedError: Non-abstract, optional attribute
+        """
+        raise NotImplementedError(
+            "{} has no attribute 'pad'".format(type(self).__name__)
+        )
+
+    @property
+    def eod(self):
+        """The EOD token id
+
+        Raises:
+            NotImplementedError: Non-abstract, optional attribute
+        """
+        raise NotImplementedError(
+            "{} has no attribute 'eod'".format(type(self).__name__)
+        )
+
+    @property
+    def bos(self):
+        """The BOS token id
+
+        Raises:
+            NotImplementedError: Non-abstract, optional attribute
+        """
+        raise NotImplementedError(
+            "{} has no attribute 'bos'".format(type(self).__name__)
+        )
+
+    @property
+    def eos(self):
+        """The EOS token id
+
+        Raises:
+            NotImplementedError: Non-abstract, optional attribute
+        """
+        raise NotImplementedError(
+            "{} has no attribute 'eos'".format(type(self).__name__)
+        )
+
+    @property
+    def mask(self):
+        """The MASK token id
+
+        Raises:
+            NotImplementedError: Non-abstract, optional attribute
+        """
+        raise NotImplementedError(
+            "{} has no attribute 'mask'".format(type(self).__name__)
+        )
+
+
+class HuggingFaceTokenizer(BaseTokenizer):
+    def __init__(self, config: TokenizerConfig, **kwargs):
+        super().__init__(config.tokenizer_model, **kwargs)
+        self.pretrained_model_name_or_path = config.tokenizer_model
+        self.kwargs = kwargs
+        self._tokenizer = None
+        self._vocab = None
+        self._inv_vocab = None
+
+    def _build_hugging_face(self):
+        if self._tokenizer is None:
+            # TODO(bnorick): download tokenizer once to lustre and use force offline to make sure all tasks read it from there
+            self._tokenizer = transformers.AutoTokenizer.from_pretrained(
+                pretrained_model_name_or_path=self.pretrained_model_name_or_path,
+                trust_remote_code=True,
+                **self.kwargs
+            )
+            self._vocab = self._tokenizer.get_vocab()
+            self._inv_vocab = {
+                token_id: token for token, token_id in self._vocab.items()
+            }
+
+    @property
+    def vocab_size(self):
+        self._build_hugging_face()
+        return len(self._tokenizer)
+
+    @property
+    def vocab(self):
+        self._build_hugging_face()
+        """Dictionary from vocab text token to id token."""
+        return self._vocab
+
+    @property
+    def inv_vocab(self):
+        self._build_hugging_face()
+        """Dictionary from vocab id token to text token."""
+        return self._inv_vocab
+
+    @property
+    def decoder(self):
+        self._build_hugging_face()
+        return self._inv_vocab
+
+    def tokenize(self, inputs, **kwargs):
+        self._build_hugging_face()
+        return self._tokenizer(inputs, **kwargs).input_ids
+
+    def detokenize(self, token_ids, **kwargs):
+        self._build_hugging_face()
+        return self._tokenizer.decode(token_ids, **kwargs)
+
+    @property
+    def eos(self):
+        self._build_hugging_face()
+        return self._tokenizer.eos_token_id
+
+    @property
+    def bos(self):
+        self._build_hugging_face()
+        return self._tokenizer.bos_token_id
+
+    @property
+    def pad(self):
+        self._build_hugging_face()
+        return self._tokenizer.pad_token_id
+
+
+class BestRQTokenizer(BaseTokenizer):
+    def __init__(self, config: TokenizerConfig, **kwargs):
+        super().__init__(f"BestRQ-{config.tokenizer_bestrq_init_method}-init", **kwargs)
+        self.kwargs = kwargs
+        self.config = config
+        self._quantizer = None
+        self._codebook = None
+
+    def _build_quantizer_and_codebook(self):
+        if self._quantizer is None:
+            self._quantizer = torch.nn.parameter.Parameter(
+                torch.empty(self.config.tokenizer_bestrq_input_size,
+                            self.config.tokenizer_bestrq_emb_size),
+                requires_grad=False,
+            )
+            self._codebook = torch.nn.parameter.Parameter(
+                torch.empty(self.config.tokenizer_bestrq_vocab_size,
+                            self.config.tokenizer_bestrq_emb_size),
+                requires_grad=False,
+            )
+            if self.config.tokenizer_bestrq_init_method == "default":
+                # default initialization follows best-rq https://arxiv.org/pdf/2202.01855
+                g = torch.Generator().manual_seed(self.config.tokenizer_bestrq_init_seed)
+                torch.nn.init.xavier_uniform_(self._quantizer, generator=g)
+                torch.nn.init.normal_(self._codebook, generator=g)
+            else:
+                raise NotImplementedError(
+                    f"Initialization method {self.config.tokenizer_bestrq_init_method} is not implemented."
+                )
+            self._codebook = F.normalize(self._codebook, dim=1, p=2, eps=1e-8)
+
+    @property
+    def vocab_size(self):
+        self._build_quantizer_and_codebook()
+        return self._codebook.size(0)
+
+    @property
+    def vocab(self):
+        """Dictionary from emb to id token."""
+        self._build_quantizer_and_codebook()
+        return None
+
+    @property
+    def inv_vocab(self):
+        """Dictionary from vocab id token to emb token."""
+        self._build_quantizer_and_codebook()
+        return self._codebook
+
+    @property
+    def decoder(self):
+        self._build_quantizer_and_codebook()
+        return self._codebook
+
+    def tokenize(self, inputs, **kwargs):
+        self._build_quantizer_and_codebook()
+        # get nearest embedding
+        xs = torch.matmul(inputs, self._quantizer.to(inputs.device))
+        xs = F.normalize(xs, dim=-1, p=2, eps=1e-8)  # [T, D]
+        codes = torch.linalg.vector_norm(
+            # [T, 1, D] - [1, V, D] -> [T, V, D]
+            xs.unsqueeze(1) - self._codebook.unsqueeze(0),
+            dim=-1, ord=2,
+        ).argmin(dim=-1).tolist()  # [T,]
+        return codes
+
+    def detokenize(self, token_ids, **kwargs):
+        self._build_quantizer_and_codebook()
+        return torch.index_select(self._codebook, dim=0, index=token_ids)
+
+    @property
+    def eos(self):
+        self._build_quantizer_and_codebook()
+        return None
+
+    @property
+    def bos(self):
+        self._build_quantizer_and_codebook()
+        return None
+
+    @property
+    def pad(self):
+        self._build_quantizer_and_codebook()
+        return None
+
+
+def build_tokenizer(args: TokenizerConfig, **kwargs):
+    """Initialize tokenizer."""
+
+    # Select and instantiate the tokenizer.
+    if args.tokenizer_type == "HuggingFaceTokenizer":
+        tokenizer = HuggingFaceTokenizer(args, **kwargs)
+    elif args.tokenizer_type == "BestRQTokenizer":
+        tokenizer = BestRQTokenizer(args, **kwargs)
+    else:
+        raise NotImplementedError(
+            "{} tokenizer is not " "implemented.".format(args.tokenizer_type)
+        )
+
+    return tokenizer
